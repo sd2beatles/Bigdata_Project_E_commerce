35ebdc236a5d44f47dd1e1fb41e9a40f6521b5ff
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2021-04-25 23:01:40 +0900

    Update flume.sh

--- Collection/flume.sh
+++ Collection/flume.sh
@@ -1,4 +1,4 @@
 :<<END
-Here,we provide a configuration file, describing a single ndoe flume deployment. 
-
+Here,we provide a configuration file, describing a single ndoe flume deployment. If you want to obtain a further information on how each configuaration is made.
+Please visit the website,https://flume.apache.org/FlumeUserGuide.html 
 END
